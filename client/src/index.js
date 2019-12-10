cdecd5db347ef09ccc9ddba4462747db63c005c6
hbw3 <hbw3@hbwebsol.com>
2019-12-10 15:51:35 +0530

    some fixes

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+//import * as serviceWorker from "./serviceWorker";
 import store from "./store";
 import { Provider } from "react-redux";
 import {
@@ -48,4 +48,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.unregister();
+//serviceWorker.unregister();
