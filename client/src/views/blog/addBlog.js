1912f6e24c1882484a6b6357f60f44fa5262488a
hbw3@hbwebsol.com <hbw3@hbwebsol.com>
2019-12-30 10:40:32 +0530

    some fixes

--- client/src/views/blog/addBlog.js
+++ client/src/views/blog/addBlog.js
@@ -11,11 +11,6 @@ import {
   Label,
   Input
 } from "reactstrap";
-/* import SunEditor from "suneditor-react";
-import "suneditor/dist/css/suneditor.min.css"; */
-
-import CKEditor from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 const AddBlog = () => {
   const [blog, setBlog] = useState({
@@ -101,52 +96,6 @@ const AddBlog = () => {
             <Row>
               <Col md={12}>
                 <Label for="description">Description</Label>
-                {/* <CKEditor
-                  editor={ClassicEditor}
-                  data="<p>Hello from CKEditor 5!</p>"
-                  onInit={editor => {
-                    // You can store the "editor" and use when it is needed.
-                    console.log("Editor is ready to use!", editor);
-                  }}
-                  onChange={(event, editor) => {
-                    const data = editor.getData();
-                    console.log({ event, editor, data });
-                  }}
-                  onBlur={(event, editor) => {
-                    console.log("Blur.", editor);
-                  }}
-                  onFocus={(event, editor) => {
-                    console.log("Focus.", editor);
-                  }}
-                /> */}
-                {/*<SunEditor
-                  setOptions={{
-                    height: "400px",
-                    buttonList: [
-                      ["undo", "redo"],
-                      ["font", "fontSize", "formatBlock"],
-                      ["bold", "underline", "italic"],
-                      ["fontColor", "hiliteColor"],
-                      ["link", "image", "video"],
-                      ["align", "horizontalRule", "list", "table"],
-                      [
-                        "outdent",
-                        "indent",
-                        "strike",
-                        "subscript",
-                        "superscript"
-                      ],
-                      ["fullScreen", "codeView"]
-                    ]
-                  }}
-                  onChange={content =>
-                    setBlog({ ...blog, ["description"]: content })
-                  }
-                  setContents={blog.description}
-                />
-                {(() => {
-                  console.log("content", blog.description);
-                })()} */}
               </Col>
             </Row>
             <Row className="mt-3">
