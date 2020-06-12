0ca48e506bc45c5adceb3ae4ed47eb46b3bafe4a
hbw2 <hbw2@hbwebsol.com>
2020-06-12 17:00:01 +0530

    UI changes of  Cart and Checkout page

--- frontend/src/main-layout/cart.js
+++ frontend/src/main-layout/cart.js
@@ -1,11 +1,20 @@
 import React, { Fragment, useState, useEffect } from "react";
-import { Box, Typography, Icon, Grid, Button } from "@material-ui/core";
+import {
+  Box,
+  Typography,
+  Icon,
+  Grid,
+  Button,
+  ButtonGroup,
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
 import PlaceHolderImg from "../assets/images/placeholder.png";
 import { connect } from "react-redux";
 
 const CartSide = (props) => {
   const [cartItems, setCartItems] = useState(props.cartValue);
+  const [subtotal, setSubTotal] = useState(0);
+  const [delievery, setDelievery] = useState(0);
 
   const removeCartItem = (removedItem) => {
     removedItem.cart = false;
@@ -19,73 +28,135 @@ const CartSide = (props) => {
     });
   };
 
+  const cartSubTotal = () => {
+    var subtotalVar = 0;
+    if (cartItems && cartItems.length) {
+      cartItems.map((item) => {
+        if (item.pricing.sellprice) {
+          var sellPrice = item.pricing.sellprice * item.cartQty;
+          subtotalVar = subtotalVar + sellPrice;
+        } else {
+          var totalPrice = item.pricing.price * item.cartQty;
+          subtotalVar = subtotalVar + totalPrice;
+        }
+      });
+    }
+    setSubTotal(subtotalVar);
+  };
+
+  const increaseItemQty = (item) => {
+    var increaseItem = item;
+    let filteredArray = cartItems.filter((cartitem) => cartitem !== item);
+    localStorage.removeItem("cartProducts");
+    increaseItem.cartQty = item.cartQty + 1;
+    filteredArray.push(increaseItem);
+    localStorage.setItem("cartProducts", JSON.stringify(filteredArray));
+    setCartItems(filteredArray);
+  };
+
+  const decreaseItemQty = (item) => {
+    if (item.cartQty <= 1) {
+      return;
+    }
+    var decreaseItem = item;
+    let filteredArray = cartItems.filter((cartitem) => cartitem !== item);
+    localStorage.removeItem("cartProducts");
+    decreaseItem.cartQty = item.cartQty - 1;
+    filteredArray.push(decreaseItem);
+    localStorage.setItem("cartProducts", JSON.stringify(filteredArray));
+    setCartItems(filteredArray);
+  };
+
+  useEffect(() => {
+    cartSubTotal();
+  }, [cartItems]);
+
   return (
     <Fragment>
       {cartItems && cartItems.length ? (
         <Fragment>
           <Box className="cart-product">
             <Box display="flex" flexDirection="column" component="div">
-              {cartItems.map((item, index) => (
-                <Box
-                  className="cart-item"
-                  key={index}
-                  style={{
-                    backgroundImage: `url(${
-                      item.feature_image
-                        ? item.feature_image.thumbnail
-                        : PlaceHolderImg
-                    })`,
-                  }}
-                >
-                  <div className="item-inner">
-                    <Icon
-                      className="remove-item"
-                      onClick={() => removeCartItem(item)}
-                    >
-                      close
-                    </Icon>
-
-                    {item.category && (
-                      <Typography variant="button" className="item-category">
-                        {item.category}
-                      </Typography>
-                    )}
-
-                    {item.name && (
-                      <Typography variant="h6" className="item-title">
-                        {item.name}
-                      </Typography>
-                    )}
-
-                    <Typography variant="body2" className="item-attr">
-                      Qty: 1
-                    </Typography>
-
-                    <Typography variant="h6" className="item-price">
-                      <span
-                        className={
-                          item.pricing.sellprice ? "has-sale-price" : ""
-                        }
+              {cartItems
+                .sort((a, b) => (a.name > b.name ? 1 : -1))
+                .map((item, index) => (
+                  <Box
+                    className="cart-item"
+                    key={index}
+                    style={{
+                      backgroundImage: `url(${
+                        item.feature_image
+                          ? item.feature_image.thumbnail
+                          : PlaceHolderImg
+                      })`,
+                    }}
+                  >
+                    <div className="item-inner">
+                      <Icon
+                        className="remove-item"
+                        onClick={() => removeCartItem(item)}
                       >
-                        ${item.pricing.price.toFixed(2)}
-                      </span>
+                        close
+                      </Icon>
 
-                      {item.pricing.sellprice ? (
-                        <span className="sale-price">
-                          ${item.pricing.sellprice.toFixed(2)}
+                      {item.category && (
+                        <Typography variant="button" className="item-category">
+                          {item.category}
+                        </Typography>
+                      )}
+
+                      {item.name && (
+                        <Typography variant="h6" className="item-title">
+                          {item.name}
+                        </Typography>
+                      )}
+
+                      <Typography
+                        variant="subtitle2"
+                        className="item-qty-wrapper"
+                      >
+                        Qty: {item.cartQty}
+                        <ButtonGroup
+                          variant="contained"
+                          color="primary"
+                          size="small"
+                          aria-label="cart-qty-button-grp"
+                          className="increse-decrease-buttongrp"
+                        >
+                          <Button onClick={() => increaseItemQty(item)}>
+                            +
+                          </Button>
+                          <Button onClick={() => decreaseItemQty(item)}>
+                            -
+                          </Button>
+                        </ButtonGroup>
+                      </Typography>
+
+                      <Typography variant="h6" className="item-price">
+                        <span
+                          className={
+                            item.pricing.sellprice ? "has-sale-price" : ""
+                          }
+                        >
+                          ${item.pricing.price.toFixed(2)}
                         </span>
-                      ) : null}
-                    </Typography>
-                    <Link
-                      to={`/product/${item.id}`}
-                      className="view-item"
-                      onClick={props.closeCart}
-                    >
-                      View
-                    </Link>
-                  </div>
-                </Box>
-              ))}
+
+                        {item.pricing.sellprice ? (
+                          <span className="sale-price">
+                            ${item.pricing.sellprice.toFixed(2)}
+                          </span>
+                        ) : null}
+                      </Typography>
+                      <Link
+                        to={`/product/${item.id}`}
+                        className="view-item"
+                        onClick={props.closeCart}
+                      >
+                        View
+                      </Link>
+                    </div>
+                  </Box>
+                ))}
             </Box>
           </Box>
 
@@ -103,7 +174,7 @@ const CartSide = (props) => {
                   </Grid>
                   <Grid item md={6} sm={6} xs={6}>
                     <Typography variant="h4" className="cart-td-right">
-                      $274.00
+                      ${subtotal}
                     </Typography>
                   </Grid>
                 </Grid>
@@ -115,7 +186,7 @@ const CartSide = (props) => {
                   </Grid>
                   <Grid item md={6} sm={6} xs={6}>
                     <Typography variant="h4" className="cart-td-right">
-                      Free
+                      {delievery === 0 ? "Free" : "$" + delievery}
                     </Typography>
                   </Grid>
                 </Grid>
@@ -127,7 +198,7 @@ const CartSide = (props) => {
                   </Grid>
                   <Grid item md={6} sm={6} xs={6}>
                     <Typography variant="h4" className="cart-td-right">
-                      $274.00
+                      ${subtotal + delievery}
                     </Typography>
                   </Grid>
                 </Grid>
@@ -147,6 +218,18 @@ const CartSide = (props) => {
       ) : (
         <Box display="flex" p={5} width="100%">
           <Typography variant="h4">Your cart is currently empty.</Typography>
+
+          <Link to="/shop">
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              className="margin-left-1"
+              onClick={props.closeCart}
+            >
+              Shop Now
+            </Button>
+          </Link>
         </Box>
       )}
     </Fragment>
