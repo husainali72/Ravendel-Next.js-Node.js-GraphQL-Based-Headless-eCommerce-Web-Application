15df22e28034a511b593fdb41edee133abaf7c22
Husain Bandookwala <husainali72@gmail.com>
2020-12-03 12:02:39 +0530

    Some UI changes

--- client/src/views/utils/loading.js
+++ client/src/views/utils/loading.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { Backdrop, CircularProgress } from "@material-ui/core";
+import { Backdrop, CircularProgress, Typography } from "@material-ui/core";
 import viewStyles from "../viewStyles.js";
 
 const Loading = () => {
   const classes = viewStyles();
   return (
     <Backdrop className={classes.backdrop} open={true}>
-      <CircularProgress color="inherit" /> Loading
+      <div className={classes.backdropInnerWrapper}>
+        <CircularProgress color="inherit" />
+        <Typography variant="h4" gutterBottom className={classes.backdropLoadingText}>
+          Loading....
+        </Typography>     
+      </div>
     </Backdrop>
   );
 };
