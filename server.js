9265b79fc2979c6bc6f4fd1c76f6b54bd97e9bb7
Aliasger <skaa120817@gmail.com>
2021-09-23 14:24:52 +0530

    server.js

--- server.js
+++ server.js
@@ -28,15 +28,7 @@ const server = new ApolloServer({
     maxFileSize: 5000000, // 5 MB
     maxFiles: 20,
   },
-  formatError: (err) => {
-    // Don't give the specific errors to the client.
-    if (err.message.startsWith('Database Error: ')) {
-      return new Error('Internal server error');
-    }
-    // Otherwise return the original error. The error can also
-    // be manipulated in other ways, as long as it's returned.
-    return err;
-  },
+
 });
 server.applyMiddleware({ app, path: "/graphql" });
 
