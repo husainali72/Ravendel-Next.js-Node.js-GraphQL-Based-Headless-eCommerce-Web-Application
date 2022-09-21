47896685f84b7582e18bb2f33da4bcd20e95d081
Taha ManaquibWala <108791205+TahaManaquibWala@users.noreply.github.com>
2022-09-21 14:37:03 +0530

    commented local database

--- config/keys.js
+++ config/keys.js
@@ -2,8 +2,8 @@
 //Staging "mongodb+srv://userMernEcommerce:passwordMernEcommerce@cluster0-nxt3c.mongodb.net/test?retryWrites=true&w=majority"
 
 //var URI = 'mongodb+srv://rv-staging:4xPAesU6bu1fELfE@ravendel-staging.2hfno.mongodb.net/rv-staging-database?retryWrites=true&w=majority'; // staging
-// var URI = 'mongodb+srv://rv-live:4xPAesU6bu1fELfE@ravendel-live.apytt.mongodb.net/rv-live-database?retryWrites=true&w=majority'; // staging
-var URI = 'mongodb+srv://ravendel:AhytMhScZbNoByBs@ravendel.k8pbl0q.mongodb.net/ravendel-database?retryWrites=true&w=majority'
+var URI = 'mongodb+srv://rv-live:4xPAesU6bu1fELfE@ravendel-live.apytt.mongodb.net/rv-live-database?retryWrites=true&w=majority'; // staging
+//var URI = 'mongodb+srv://ravendel:AhytMhScZbNoByBs@ravendel.k8pbl0q.mongodb.net/ravendel-database?retryWrites=true&w=majority'
 var accessKeyId = "AKIA5NPJTJ66BD6LZGU2";
 var secretAccessKey = "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";
 var bucketName = "revendal-image";
