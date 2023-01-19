870a21b55da4e96ff9b493ea9b38789f8f2a9768
HB WEBSOL <info@hbwebsol.com>
2023-01-19 19:03:14 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,24 +1,11 @@
-/*if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    jwtSecret: process.env.SECRET_APP_KEY
-  };
-} else {*/
-/* module.exports = {
-  //mongoURI: "mongodb+srv://ravendel-local:ravendelpassword@ravendel-local.gcmvx.gcp.mongodb.net/ravendel-local?retryWrites=true&w=majority",
-  mongoURI: "mongodb+srv://userMernEcommerce:passwordMernEcommerce@cluster0-nxt3c.mongodb.net/test?retryWrites=true&w=majority",
-  jwtSecret: "Theziner"
-}; */
-/*}*/
-
 if (process.env.NODE_ENV === "production") { 
   module.exports = {    
-    mongoURI: "mongodb+srv://userMernEcommerce:passwordMernEcommerce@cluster0-nxt3c.mongodb.net/test?retryWrites=true&w=majority",
-    jwtSecret: "Theziner"
+    mongoURI: "",
+    jwtSecret: ""
   };
 } else {
   module.exports = {
-    mongoURI: "mongodb+srv://ravendel-local:ravendelpassword@ravendel-local.gcmvx.gcp.mongodb.net/ravendel-local?retryWrites=true&w=majority",    
-    jwtSecret: "Theziner"
+    mongoURI: "",    
+    jwtSecret: ""
   };
 }
