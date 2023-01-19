9a14199947f93f02b91031af8566483da36f0c50
HB WEBSOL <info@hbwebsol.com>
2023-01-19 19:01:58 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,19 +1,13 @@
-//Prodution "mongodb+srv://userMernEcommerce:passwordMernEcommerce@cluster0-nxt3c.mongodb.net/test?retryWrites=true&w=majority",
-//Staging "mongodb+srv://userMernEcommerce:passwordMernEcommerce@cluster0-nxt3c.mongodb.net/test?retryWrites=true&w=majority"
-
-//var URI = 'mongodb+srv://rv-staging:4xPAesU6bu1fELfE@ravendel-staging.2hfno.mongodb.net/rv-staging-database?retryWrites=true&w=majority'; // staging
-var URI = 'mongodb+srv://rv-live:4xPAesU6bu1fELfE@ravendel-live.apytt.mongodb.net/rv-live-database?retryWrites=true&w=majority'; // staging
-
-//var URI = 'mongodb+srv://ravendel:AhytMhScZbNoByBs@ravendel.k8pbl0q.mongodb.net/ravendel-database?retryWrites=true&w=majority'
-var accessKeyId = "AKIA5NPJTJ66BD6LZGU2";
-var secretAccessKey = "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";
+var URI = ''; // staging
+var accessKeyId = "";
+var secretAccessKey = "";
 var bucketName = "revendal-image";
 var bucketBaseURL= `https://${bucketName}.s3.amazonaws.com/`;
 
 if (process.env.NODE_ENV === "production") {
-  URI = "mongodb+srv://rv-live:4xPAesU6bu1fELfE@ravendel-live.apytt.mongodb.net/rv-live-database?retryWrites=true&w=majority";
-  accessKeyId = "AKIA5NPJTJ66BD6LZGU2";
-  secretAccessKey = "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";
+  URI = "";
+  accessKeyId = "";
+  secretAccessKey = "";
   bucketName = "revendal-image-prod";
   bucketBaseURL= `https://${bucketName}.s3.amazonaws.com/`;
 }
@@ -21,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   mongoURI: URI,
-  jwtSecret: "Theziner",
+  jwtSecret: "",
   JWT_ACCESS_EXPIRATION_MINUTES: 30,
   JWT_REFRESH_EXPIRATION_DAYS: 30,
   resetPasswordExpirationMinutes: 10,
