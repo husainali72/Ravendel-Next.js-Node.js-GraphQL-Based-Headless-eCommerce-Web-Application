013d5f34ff85146d755365cebd9639d44e84ce2d
Husain Bandookwala <husainali72@gmail.com>
2023-12-12 19:05:05 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,10 +1,10 @@
 // export const IMAGE_BASE_URL = "https://ravendel.herokuapp.com";
-export const IMAGE_BASE_URL = "https://ravendel-node.onrender.com";
+export const IMAGE_BASE_URL = "http://demo1.ravendel.io";
 // export const IMAGE_BASE_URL = "http://localhost:3000"
 
 
 // export const API_BASE_URL = "http://localhost:3001/api";
-export const API_BASE_URL = "https://ravendel-node.onrender.com/api";
+export const API_BASE_URL = "http://demo1.ravendel.io/api";
 
 /* -------------------------------  imageURl changes to bucketURL ------------------------- */
 
