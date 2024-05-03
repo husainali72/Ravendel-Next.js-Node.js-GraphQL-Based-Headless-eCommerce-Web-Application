ed4442a1ea0a6c5e853989c34bbee073e36a88c0
Husain Bandookwala <husainali72@gmail.com>
2024-05-03 23:36:51 +0530

    Update homebanner.js

--- frontend/components/banner/homebanner.js
+++ frontend/components/banner/homebanner.js
@@ -20,11 +20,11 @@ const Homebanner = ({ settings, slider }) => {
       <Carousel>
         {slider.map((slide, i) => (
           <Carousel.Item key={i}>
-            <a
-              href={slide.link}
-              target={slide.open_in_tab ? "_blank" : null}
-              rel={slide.open_in_tab ? "noreferrer noopener" : 'noreferrer'}
-            >
+            // <a
+            //   href={slide.link}
+            //   target={slide.open_in_tab ? "_blank" : null}
+            //   rel={slide.open_in_tab ? "noreferrer noopener" : 'noreferrer'}
+            // >
               <Image
                 src={
                   imageSrc
@@ -38,7 +38,7 @@ const Homebanner = ({ settings, slider }) => {
                 alt={`slider`}
                 onError={handleImageError}
               />
-            </a>
+            // </a>
           </Carousel.Item>
         ))}
       </Carousel>
