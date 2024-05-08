5a8377fbe9f21a7921fc0c7ec7b1ba87677d2812
husainAli72 <husainali72@gmail.com>
2024-05-08 15:06:49 +0530

    home-page enhancement

--- frontend/components/category/category.js
+++ frontend/components/category/category.js
@@ -37,9 +37,9 @@ const Category = ({ category }) => {
           />
         ) : null}
         <div>
-          <h4 className="theme-color my-3">
-            Products <span className="black-color">Category</span>
-          </h4>
+          <h5 className="black-color my-3">
+            Products Category
+          </h5>
           <div
             className={
               showSlider
