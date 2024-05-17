a64721ce373000fe765b4514966b0f85c9661026
keshu-g <kartikgoswami082@gmail.com>
2024-05-17 14:27:38 +0530

    Added addressType in addressBook

--- gqschema/customer.js
+++ gqschema/customer.js
@@ -14,6 +14,11 @@ module.exports = `
     gender: String
   }
 
+  enum ADDRESS_TYPE {
+    homeAddress
+    officeAddress
+  }
+
   type AddressBook {
     id: ID
     _id: ID
@@ -23,6 +28,7 @@ module.exports = `
     phone: String
     addressLine1: String
     addressLine2: String
+    addressType: ADDRESS_TYPE
     city: String
     country: String
     state: String
@@ -94,6 +100,7 @@ module.exports = `
       phone: String
       addressLine1: String
       addressLine2: String
+      addressType: ADDRESS_TYPE
       city: String
       country: String
       state: String
@@ -109,6 +116,7 @@ module.exports = `
       phone: String
       addressLine1: String
       addressLine2: String
+      addressType: ADDRESS_TYPE
       city: String
       country: String
       state: String
