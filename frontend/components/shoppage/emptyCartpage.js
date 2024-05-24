a305d522096605644c480d1b998f9687ae31a7d8
husainali72 <husainali72@gmail.com>
2024-05-24 16:32:23 +0530

    Created empty cart page ui

---
+++ frontend/components/shoppage/emptyCartpage.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import emptyCart from "../../components/images/emptyCart.png";
+const EmptyCart = () => {
+  return (
+    <>
+      <div className="emptyCart">
+        <div>
+          <img src={emptyCart.src} />
+          <h5>Your cart is empty</h5>
+          <p>Just relax, let us help you find some first class products</p>
+          <div className="card-btons text-align-center outline">
+          <Link href="/"> Start Shopping</Link>
+        </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default EmptyCart;
