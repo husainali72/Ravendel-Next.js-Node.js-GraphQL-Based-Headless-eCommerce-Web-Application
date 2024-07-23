b9e525d651980d497442a6c5f2152981a1f96961
husainAli72 <husainali72@gmail.com>
2024-07-23 16:00:14 +0530

    Created support pages

---
+++ frontend/pages/privacy-policy.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <div className='support-page-wrapper'>
+      <div className='container'>
+        <h1>Privacy Policy</h1>
+        <h2>Introduction</h2>
+        <p>Welcome to Zemjet. We are committed to protecting your privacy and ensuring that your personal information is handled safely and responsibly. This Privacy Policy describes how we collect, use, and protect your information when you visit our website <a href='http://www.zemjet.com'>www.zemjet.com</a> and make use of our services.</p>
+        <h2>1. Information We Collect</h2>
+        <p>We collect several types of information for various purposes to provide and improve our services to you.</p>
+        <h3>Personal Information:</h3>
+        <ul>
+          <li>Name</li>
+          <li>Email address</li>
+          <li>Phone number</li>
+          <li>Billing and shipping address</li>
+          <li>Payment information</li>
+        </ul>
+        <h3>Non-Personal Information:</h3>
+        <ul>
+          <li>Browser type</li>
+          <li>Internet Protocol (IP) address</li>
+          <li>Time zone setting</li>
+          <li>Referring/exit pages</li>
+          <li>Operating system</li>
+        </ul>
+        <h2>2. How We Use Your Information</h2>
+        <p>Zemjet uses the collected data for various purposes:</p>
+        <ul>
+          <li>To process and fulfill your orders</li>
+          <li>To improve our website and services</li>
+          <li>To communicate with you, including responding to inquiries and sending promotional offers</li>
+          <li>To detect, prevent, and address technical issues or fraud</li>
+        </ul>
+        <h2>3. How We Share Your Information</h2>
+        <p>We do not sell, trade, or otherwise transfer your personal information to third parties without your consent, except as described below:</p>
+        <ul>
+          <li><b>Service Providers:</b>We may share your information with third-party service providers to perform certain services on our behalf, such as payment processing and order fulfillment.</li>
+          <li><b>Legal Requirements</b>We may disclose your personal information to comply with a legal obligation, protect our rights, or prevent fraud or abuse of our services.</li>
+        </ul>
+        <h2>4. Cookies</h2>
+        <p>Zemjet uses cookies to enhance your experience on our website. Cookies are small data files stored on your device. You can choose to accept or decline cookies, but this may prevent you from taking full advantage of the website.</p>
+        <h2>5. Security </h2>
+        <p>We take appropriate security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. However, no method of transmission over the Internet or method of electronic storage is 100% secure.</p>
+        <h2>6. Your Rights</h2>
+        <p>You have the right to access, update, or delete the personal information we hold about you. If you wish to exercise any of these rights, please get in touch with us.</p>
+        <h2>7. Changes to This Privacy Policy</h2>
+        <p>Zemjet may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes.</p>
+        <h2>8. Contact Us</h2>
+        <p>If you have any questions or concerns about this Privacy Policy, please contact us at:</p>
+        <p>Email: <a href='mailto:sales@zemjet.com'>sales@zemjet.com</a> </p>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
---
+++ frontend/pages/terms-conditions.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const TermsCondition = () => {
+  return (
+    <div className='support-page-wrapper'>
+      <div className='container'>
+        <h1>Terms and Conditions</h1>
+        <h2>Welcome to Zemjet</h2>
+        <p>These Terms and Conditions outline the rules and regulations for using Zemjet's website, <a href='http://www.zemjet.com'>www.zemjet.com</a>.</p>
+        <p>By accessing this website, we assume you accept these terms and conditions. Do not continue to use Zemjet if you do not agree to take all of the terms and conditions stated on this page</p>
+        <h2>1. Introduction</h2>
+        <p>These terms and conditions govern your use of our website and services. By using our website, you agree to these terms in full. If you disagree with any part of these terms and conditions, do not use our website.</p>
+        <h2>2. Intellectual Property Rights</h2>
+        <p>Unless otherwise stated, Zemjet and/or its licensors own the intellectual property rights for all material on Zemjet. All intellectual property rights are reserved. You may access this from Zemjet for your own personal use, subject to restrictions set in these terms and conditions.</p>
+        <h3>You must not:</h3>
+        <ul>
+          <li>Republish material from Zemjet</li>
+          <li>Sell, rent, or sub-license material from Zemjet</li>
+          <li>Reproduce, duplicate, or copy material from Zemjet</li>
+          <li>Redistribute content from Zemjet</li>
+        </ul>
+        <h2>3. User Account</h2>
+        <ul>
+          <li>You may be required to create an account to use certain features of our website. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer or device.</li>
+          <li>You agree to accept responsibility for all activities that occur under your account or password.</li>
+          <li>Zemjet reserves the right to refuse service, terminate accounts, remove or edit content, or cancel orders at our discretion.</li>
+        </ul>
+        <h2>4. Order and Payment</h2>
+        <ul>
+          <li>By placing an order on our website, you agree to provide current, complete, and accurate purchase and account information.</li>
+          <li>All payments are processed through secure gateways and are subject to validation checks and authorization by the card issuer.</li>
+          <li>We reserve the right to refuse or cancel any order if fraud or an unauthorized or illegal transaction is suspected.</li>
+        </ul>
+        <h2>5. Shipping and Delivery</h2>
+        <ul>
+          <li>Shipping times may vary depending on the destination and shipping method selected.</li>
+          <li>Zemjet is not responsible for delays caused by shipping carriers or events beyond our control.</li>
+          <li>Risk of loss and title for products purchased from Zemjet pass to you upon delivery to the carrier.</li>
+        </ul>
+        <h2>6. Returns and Refunds</h2>
+        <ul>
+          <li>Please review our Returns and Refunds Policy for details on how to return products and obtain refunds.</li>
+          <li>To be eligible for a return, items must be unused and in the same condition that you received them.</li>
+        </ul>
+        <h2>7. Limitation of Liability</h2>
+        <ul>
+          <li>Zemjet will not be liable for any damages arising from the use or inability to use our website or services.</li>
+          <li>Zemjet does not warrant that the website will be uninterrupted or error-free.</li>
+        </ul>
+        <h2>8. Governing Law</h2>
+        <p>These terms and conditions are governed by and construed in accordance with the laws of Indian Government, and you irrevocably submit to the exclusive jurisdiction of the courts in that location.</p>
+        <h2>9. Changes to Terms</h2>
+        <p>Zemjet reserves the right to revise these terms and conditions at any time. By using this website, you agree to be bound by the current version of these terms and conditions.</p>
+        <h2>10. Contact Us</h2>
+        <p>If you have any questions about these terms and conditions, please contact us at <a href='mailto:sales@zemjet.com'>sales@zemjet.com</a>.</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default TermsCondition
